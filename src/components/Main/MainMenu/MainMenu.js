3e624c63175d4088ce046bc821236f5f0db09af3
Yuri Podolsky <podolsky4@gmail.com>
2020-12-02 15:21:05 +0200

    Perform indentation in MainMenu.js
    Signed-off-by: Yuri Podolsky <podolsky4@gmail.com>

--- src/components/Main/MainMenu/MainMenu.js
+++ src/components/Main/MainMenu/MainMenu.js
@@ -11,76 +11,72 @@ const MainMenu = ({path, dark, openLoginForm, openSignUpTalent, openSignUpEmploy
   const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
 
   let grayMode = !(path === routes.COMMON.ROOT ||
-      path === routes.COMMON.FOR_TALENTS ||
-      path === routes.COMMON.FOR_EMPLOYERS || path === routes.COMMON.ABOUT ||
-      path === routes.COMMON.BLOG);
+        path === routes.COMMON.FOR_TALENTS ||
+        path === routes.COMMON.FOR_EMPLOYERS || path === routes.COMMON.ABOUT ||
+        path === routes.COMMON.BLOG);
 
   return (
-      <div className={`menuContainer ${grayMode ? 'gray' : ''}`}>
-        {mobileMenuVisible && <MobileMenu
-            openLoginForm={openLoginForm}
-            openSignUpTalent={openSignUpTalent}
-            openSignUpEmployer={openSignUpEmployer}
-            closeMobileMenu={()=>setMobileMenuVisible(false)}
-        />}
-        <div className='logoContainer'>
-          <Link to={routes.COMMON.ROOT} className='logo'>
-            Mangostart
-          </Link>
-        </div>
-        <ul className='menuItemsContainer'>
-          <li>
-            <NavLink to={routes.COMMON.FOR_TALENTS}
-                     activeClassName={'menuItem active'}
-                     className={`menuItem ${grayMode && 'gray'}`}>
-              Talents
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={routes.COMMON.FOR_EMPLOYERS}
-                     activeClassName={'menuItem active'}
-                     className={`menuItem ${grayMode && 'gray'}`}>
-              Employers
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={routes.COMMON.FIND_JOB}
-                     activeClassName={'menuItem active'}
-                     className={`menuItem ${grayMode && 'gray'}`}>
-              Jobs
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={routes.COMMON.BLOG}
-                     activeClassName={'menuItem active'}
-                     className={`menuItem ${grayMode && 'gray'}`}>
-              Blog
-            </NavLink>
-          </li>
-        </ul>
-        <div className='menuItemsContainer'>
-          <div onClick={openLoginForm}>
-            <span className={`menuItem  ${grayMode &&
-            'gray'} authBlock left`}>Log in</span>
+        <div className={`menuContainer ${grayMode ? 'gray' : ''}`}>
+          {mobileMenuVisible && <MobileMenu
+                openLoginForm={openLoginForm}
+                openSignUpTalent={openSignUpTalent}
+                openSignUpEmployer={openSignUpEmployer}
+                closeMobileMenu={() => setMobileMenuVisible(false)}
+          />}
+          <div className='logoContainer'>
+            <Link to={routes.COMMON.ROOT} className='logo'>
+              Mangostart
+            </Link>
           </div>
-          <div className='sign-container'>
-              <span className={`menuItem  ${grayMode &&
-              'gray'} authBlock`}>
+          <ul className='menuItemsContainer'>
+            <li>
+              <NavLink to={routes.COMMON.FOR_TALENTS}
+                       activeClassName={'menuItem active'}
+                       className={`menuItem ${grayMode && 'gray'}`}>
+                Talents
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={routes.COMMON.FOR_EMPLOYERS}
+                       activeClassName={'menuItem active'}
+                       className={`menuItem ${grayMode && 'gray'}`}>
+                Employers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={routes.COMMON.FIND_JOB}
+                       activeClassName={'menuItem active'}
+                       className={`menuItem ${grayMode && 'gray'}`}>
+                Jobs
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={routes.COMMON.BLOG}
+                       activeClassName={'menuItem active'}
+                       className={`menuItem ${grayMode && 'gray'}`}>
+                Blog
+              </NavLink>
+            </li>
+          </ul>
+          <div className='menuItemsContainer'>
+            <div onClick={openLoginForm}>
+            <span className={`menuItem  ${grayMode && 'gray'} authBlock left`}>Log in</span>
+            </div>
+            <div className='sign-container'>
+              <span className={`menuItem  ${grayMode && 'gray'} authBlock`}>
                 Sign up
-                <ul className={`dropdownMenu ${!dropdownVisible &&
-                'hidden'} ${grayMode && 'gray'}`}
-                >
+                <ul className={`dropdownMenu ${!dropdownVisible && 'hidden'} ${grayMode && 'gray'}`}>
                     <li onClick={openSignUpTalent}>Talents</li>
                     <li onClick={openSignUpEmployer}>Employers</li>
                 </ul>
               </span>
               <div onClick={() => setDropdownVisible(!dropdownVisible)}>
-                  <Down active={dropdownVisible} grayMode={grayMode}/>
+                <Down active={dropdownVisible} grayMode={grayMode}/>
               </div>
+            </div>
           </div>
+          <BurgerMenu onClick={() => setMobileMenuVisible(!mobileMenuVisible)} grey={grayMode}/>
         </div>
-        <BurgerMenu onClick={() => setMobileMenuVisible(!mobileMenuVisible)} grey={grayMode}/>
-      </div>
   );
 };
 
