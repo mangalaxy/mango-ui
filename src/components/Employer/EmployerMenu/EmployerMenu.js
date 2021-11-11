4f50a0d96fb0c385ee8159df73c56da3f48430db
DedNikolai <38811381+DedNikolai@users.noreply.github.com>
2021-11-11 22:47:48 +0200

    get-employer-company (#28)

--- src/components/Employer/EmployerMenu/EmployerMenu.js
+++ src/components/Employer/EmployerMenu/EmployerMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import routes from '../../../constants/routes.json';
 import './EmloyerMenu.scss';
@@ -6,63 +6,76 @@ import loginService from '../../../services/loginService'
 import {useHistory} from 'react-router';
 import {logout} from '../../../actions/userActions';
 import {connect} from 'react-redux';
+import EmployerMobileMenu from './EmployerMobileMenu/EmployerMobileMenu'
+import BurgerMenu from '../../../components/Main/MainMenu/BurgerMenu/BurgerMenu';
 
 const EmployerMenu = ({user,logoutUser}) => {
+  const [isMobileMenuOpen, toggleMobileMenu] = useState(false);
   const history = useHistory();
   const goMain=()=>{
     logoutUser();
     history.replace(routes.COMMON.ROOT)
+  };
+
+
+  if (isMobileMenuOpen) {
+      return <EmployerMobileMenu user={user} logoutUser={()=>loginService.logout(goMain)} closeMobile={(flag) => toggleMobileMenu(flag)} />
   }
 
   return (
-      <div className='employerMenuContainer'>
-        <div>
-          <Link to={routes.EMPLOYER.HOME} className='logo'>
-            mangostart
-          </Link>
-        </div>
-        <ul className='menuItemsContainer'>
-          <li>
-            <NavLink to={routes.EMPLOYER.POSITIONS}
-                     className='menuItem'
-                     activeClassName={'menuItem active'}>
-              My positions
-            </NavLink>
-          </li>
+      <Fragment>
+          <div className='employerMenuContainer'>
+              <div className='employerLogoContainer'>
+                  <Link to={routes.EMPLOYER.HOME} className='logo'>
+                      mangostart
+                  </Link>
+                  <div className='buregrMenuContainer'>
+                      <BurgerMenu purple onClick={() => toggleMobileMenu(true)}/>
+                  </div>
+              </div>
+              <ul className='menuItemsContainer'>
+                  <li className='itemLink'>
+                      <NavLink to={routes.EMPLOYER.POSITIONS}
+                               className='menuItem'
+                               activeClassName={'menuItem active'}>
+                          My positions
+                      </NavLink>
+                  </li>
 
-          <li>
-            <NavLink to={routes.EMPLOYER.BOOKMARKED}
-                     className='menuItem'
-                     activeClassName={'menuItem active'}>
-              Bookmarked
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={routes.EMPLOYER.INTERVIEWS}
-                     className='menuItem'
-                     activeClassName={'menuItem active'}>
-              Interviews
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={routes.EMPLOYER.COMPANY}
-                     className='menuItem'
-                     activeClassName={'menuItem active'}>
-              My company
-            </NavLink>
-          </li>
-        </ul>
-        <div className='menuItemsContainer'>
-          <div>
-            <span className='menuItem authBlock left'>
-              {user && user.fullName}
-            </span>
+                  <li className='itemLink'>
+                      <NavLink to={routes.EMPLOYER.BOOKMARKED}
+                               className='menuItem'
+                               activeClassName={'menuItem active'}>
+                          Bookmarked
+                      </NavLink>
+                  </li>
+                  <li className='itemLink'>
+                      <NavLink to={routes.EMPLOYER.INTERVIEWS}
+                               className='menuItem'
+                               activeClassName={'menuItem active'}>
+                          Interviews
+                      </NavLink>
+                  </li>
+                  <li className='itemLink'>
+                      <NavLink to={routes.EMPLOYER.COMPANY}
+                               className='menuItem'
+                               activeClassName={'menuItem active'}>
+                          My company
+                      </NavLink>
+                  </li>
+              </ul>
+              <div className='looutContainer'>
+                  <div>
+                    <span className='menuItem authBlock left'>
+                      {user && user.fullName}
+                    </span>
+                  </div>
+                  <div>
+                      <span className='menuItem authBlock' onClick={()=>loginService.logout(goMain)}>Sign out</span>
+                  </div>
+              </div>
           </div>
-          <div>
-            <span className='menuItem authBlock' onClick={()=>loginService.logout(goMain)}>Sign out</span>
-          </div>
-        </div>
-      </div>
+      </Fragment>
   )
 }
 
